7494ad6a2da1e6d83714b1aca51775ce8ed2d82d
Andrew Newhouse <andrewwn@gmail.com>
2018-09-01 10:26:29 -0400

    starting site -- no issues

---
+++ script/_serve_dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# terminate the script if any command fails
+set -e
+
+# Run JS build task
+#npm run build:js
+
+# Start jekyll server
+bundle exec jekyll serve \
+  --config _config.yml \
+  --host 0.0.0.0 \
+  --livereload \
+  --watch \
+  --incremental
